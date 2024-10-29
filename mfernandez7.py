70f40559b660b13b8fe7f6a51e1da5a218cdb808
moises-121 <fernandmoises95@gmail.com>
2024-10-29 04:10:12 +0000

    Lab #7 First Version

---
+++ mfernandez7.py
@@ -0,0 +1,34 @@
+#Moises Fernandez
+
+integer_var = 7
+float_var = 3.14
+string_var = "Hello"
+
+if integer_var > 5:
+	print(f"The integer variable {integer_var} is greater than 5.")
+
+elif integer_var < 5:
+	print(f"The integer variable {integer_var} is less than 5.")
+
+else:
+	print(f"The integer variable {integer_var} is equal to 5.")
+
+
+if float_var > 2.5:
+	print(f"The float variable {float_var} is greater than 2.5.")
+
+elif float_var < 2.5:
+
+	print(f"The float variable {float_var} is less than 2.5.")
+
+else:
+	print(f"The float variable {float_var} is equal to 2.5.")
+
+
+if string_var == "Hello":
+
+	print(f"The string variable is equal to 'Hello'.")
+
+elif string_var != "Hello":
+
+	print(f"The string variable is not equal to 'Hello'.")
